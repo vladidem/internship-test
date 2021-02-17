430fce0a54dbade28346f384954c5b1849853afe
vladem <lairotele@gmail.com>
2021-02-17 14:28:40 +0400

    Fix formatting, replace field with get only property

--- PageStatistics/Services/PageWordCounter.cs
+++ PageStatistics/Services/PageWordCounter.cs
@@ -1,34 +1,56 @@
 using System;
 using System.Collections.Generic;
 using System.CommandLine;
-using System.Linq;
 
 namespace PageStatistics.Services
 {
-    public class PageWordCounter: IPageWordCounter
+    public class PageWordCounter : IPageWordCounter
     {
-        private static readonly Char[] Delimiters = {' ', ',', '.', '!', '?', '"', ';', ':', '[', ']', '(', ')', '\n', '\r', '\t', '\''};
+        private static readonly char[] Delimiters =
+        {
+            ' ',
+            '\n',
+            '\r',
+            '\t',
+            ',',
+            '.',
+            '!',
+            '?',
+            '"',
+            '\'',
+            ';',
+            ':',
+            '[',
+            ']',
+            '(',
+            ')',
+            '^',
+            '&',
+            '\\',
+            '/',
+            '-'
+        };
 
-        private readonly Dictionary<string, int> _statistics;
         private readonly IConsole _console;
 
         public PageWordCounter(IConsole console)
         {
             _console = console;
-            _statistics = new Dictionary<string, int>();
+            Statistics = new Dictionary<string, int>();
         }
 
         public void AddText(string text)
         {
-            var words = text.Split(Delimiters);
+            var words = text.Split(Delimiters, StringSplitOptions.RemoveEmptyEntries);
 
             foreach (var word in words)
             {
-                var currentCount = _statistics.ContainsKey(word.ToLower()) ? _statistics[word.ToLower()] : 0;
-                _statistics[word.ToLower()] = currentCount + 1;
+                var normalizedWord = word.ToLower();
+                var currentCount = Statistics.ContainsKey(normalizedWord) ? Statistics[normalizedWord] : 0;
+                Statistics[normalizedWord] = currentCount + 1;
             }
         }
 
-        public Dictionary<string, int> Statistics => _statistics;
+        public Dictionary<string, int> Statistics { get; }
     }
 }
