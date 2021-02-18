b1b31be75c1f18aecc9b335441659459bfdd2136
vladem <lairotele@gmail.com>
2021-02-18 12:36:06 +0400

    Add models to dbcontext

--- PageStatistics/IPageStatisticsDbContext.cs
+++ PageStatistics/IPageStatisticsDbContext.cs
@@ -1,10 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using PageStatistics.Models;
 
 namespace PageStatistics
 {
     public interface IPageStatisticsDbContext
     {
-        public DatabaseFacade Database { get; }
+        DbSet<Page> Pages { get; set; }
+
+        DbSet<Word> Words { get; set; }
+
+        DbSet<WordFrequency> WordFrequencies { get; set; }
+
+        DatabaseFacade Database { get; }
+
+        int SaveChanges();
     }
 }
--- PageStatistics/PageStatisticsDbContext.cs
+++ PageStatistics/PageStatisticsDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using PageStatistics.Models;
 
 namespace PageStatistics
 {
@@ -7,5 +8,11 @@ namespace PageStatistics
         public PageStatisticsDbContext(DbContextOptions options) : base(options)
         {
         }
+
+        public DbSet<Page> Pages { get; set; }
+
+        public DbSet<Word> Words { get; set; }
+
+        public DbSet<WordFrequency> WordFrequencies { get; set; }
     }
 }
